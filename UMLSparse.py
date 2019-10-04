43b20da39a5ba7c928fb7b2712bad2d098e6fcad
kperera2019 <kevin.perera13@gmail.com>
2019-10-04 14:52:21 -0400

    refactoring codebase

--- UMLSparse.py
+++
@@ -1,6 +0,0 @@
-from owlready2 import *
-from owlready2.pymedtermino2 import *
-from owlready2.pymedtermino2.umls import *
-default_world.set_backend(filename = "pym.sqlite3")
-import_umls("umls-2019AA-metathesaurus.zip", terminologies = ["ICD10", "SNOMEDCT_US", "CUI"])
-default_world.save()
