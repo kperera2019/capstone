54857c7793814eba472cface0c4966b849b904f0
kperera2019 <kevin.perera13@gmail.com>
2019-12-06 13:41:42 -0500

    settled on naming conventions

--- generateconfig.py
+++ generateconfig.py
@@ -5,11 +5,11 @@ import os
 import subprocess
 
 Config = configparser.ConfigParser()
-Config['TestOne'] = {'TestVariety': 'UMLS', 'Test': 'Synonym','InputType': 'D:/capstone/embeddings/wemb_sample.tsv','WordLength': '20','VectorLength':'100'}
+Config['TestOne'] = {'TestVariety': 'UMLS', 'Test': 'Semantics','InputType': 'D:/capstone/embeddings/wemb_sample.tsv','WordLength': '20','VectorLength':'100'}
 
 Config['TestTwo'] = {'TestVariety':'MIMIC', 'Test':'NeuralNetwork', 'InputOne':'D:/capstone/embeddings/wemb_sample.tsv','InputTwo':'D:/capstone/mimic/INPUTS/DIAGNOSES_ICD.csv','InputThree':'D:/capstone/mimic/INPUTS/NOTEEVENTS.csv'}
 
-Config['TestThree'] = {'TestVariety':'UMLS','Test':'Semantics', 'InputType': 'D:/capstone/embeddings/wemb_sample.tsv','WordLength': '20','VectorLength':'100'}
+Config['TestThree'] = {'TestVariety':'UMLS','Test':'Synonyms', 'InputType': 'D:/capstone/embeddings/wemb_sample.tsv','WordLength': '20','VectorLength':'100'}
 with open('example.ini', 'w') as configfile:
 	Config.write(configfile)
 
